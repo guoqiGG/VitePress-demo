8081a4b7c9adbdb4d1934dc10248061b819e8846
guoqiGG <2411074379@qq.com>
2024-04-11 12:34:02 +0800

    1

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -29,7 +29,7 @@ export default defineConfig({
     algolia: {
       appId: "KM9NFXCSIQ",
       apiKey: "304298ae0579ca1480b39bdc0eb62aa1",
-      indexName: "Test",
+      indexName: "index",
       placeholder: "请输入关键词",
       buttonText: "搜索",
     },
